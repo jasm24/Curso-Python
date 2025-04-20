e33812c4a7844d8087868edd53f0ed7c76c18103
jasm24 <jasm24@gmail.com>
2025-04-20 21:03:42 +0200

    2004

--- 01-variables.py
+++ 01-variables.py
@@ -1,5 +1,11 @@
 variable = "Hola Mundo"
+print(variable)
 numero = 42
+print(numero)
 decimal = 10.5
+print(decimal)
 verdadero = True
+print(verdadero)
 falso = False
+print(falso)
+print(variable, numero, decimal, verdadero, falso)
